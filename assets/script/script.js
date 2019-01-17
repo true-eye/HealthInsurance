61b33c99173ab14f7c3558590d16fa23a9cd88f3
AngularMaster <>
2019-01-17 10:55:55 -0400

    until page 4

--- assets/script/script.js
+++ assets/script/script.js
@@ -23,3 +23,20 @@ document.getElementById("btn-page-4").addEventListener("click", function(event){
     document.getElementById("div-caption").setAttribute("style", "display: none;");
     document.getElementById("page-5").setAttribute("style", "display: inline;");
 });
+
+document.getElementById("radio-male").addEventListener("click", function(event){
+    if( document.getElementById("radio-male").getAttribute("checked") != "checked")
+    {
+        document.getElementById("radio-male").setAttribute("checked", "checked");
+        document.getElementById("radio-female").removeAttribute("checked");
+    }
+});
+
+
+document.getElementById("radio-female").addEventListener("click", function(event){
+    if( document.getElementById("radio-female").getAttribute("checked") != "checked")
+    {
+        document.getElementById("radio-female").setAttribute("checked", "checked");
+        document.getElementById("radio-male").removeAttribute("checked");
+    }
+});
